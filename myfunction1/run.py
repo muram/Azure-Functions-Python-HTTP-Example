392e59a5061229adcc8fa0948ed8cddfa0183525
U-ALERTLOGIC\welly.siauw <welly.siauw@AL-HOU-10520.alertlogic.net>
2017-07-10 23:01:53 -0500

    Update run.py

--- myfunction1/run.py
+++ myfunction1/run.py
@@ -7,6 +7,7 @@
 
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'lib')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'env/Lib/site-packages')))
 
 import json, requests
 from AzureHTTPHelper import HTTPHelper
